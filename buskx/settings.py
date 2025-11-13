2dfa80a495106e9020d29064cc7a696f960e56c5
Qynixor <kisoroashraf@gmail.com>
2025-11-13 04:25:03 -0800

    Fix: add campaign-platform-kmv9.onrender.com to ALLOWED_HOSTS

--- buskx/settings.py
+++ buskx/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security settings
 SECRET_KEY = env('SECRET_KEY', default='unsafe-secret-key')
 DEBUG = env.bool('DEBUG', default=True)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['www.rallynex.com','localhost', '127.0.0.1','rallynex1.onrender.com',])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['www.rallynex.com','localhost', '127.0.0.1','rallynex1.onrender.com', 'campaign-platform-kmv9.onrender.com', ])
 
 
 
@@ -123,6 +123,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://www.rallynex.com',
     'https://rallynex.com',
     'https://rallynex1.onrender.com',
+    'https://campaign-platform-kmv9.onrender.com', 
 ]
 
 
