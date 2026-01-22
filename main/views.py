37cb2d07d68a4b4fe0ea5840adb3ba209f92c703
Qynixor <kisoroashraf@gmail.com>
2026-01-22 19:17:47 +0300

    my commit

--- main/views.py
+++ main/views.py
@@ -123,7 +123,8 @@ Disallow: /accounts/
 Disallow: /tinymce/
 Disallow: /static/
 
-Sitemap: https://rallynex.com/sitemap.xml
+Sitemap: https://www.rallynex.com/sitemap.xml
+
 """
     return HttpResponse(content, content_type="text/plain")
 
